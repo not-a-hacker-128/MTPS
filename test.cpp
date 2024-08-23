9d41ac8a85ac63ba515000845136709553854110
no-hacker-man-128 <nohackerman89@proton.me>
2024-08-23 16:49:45 +0200

    creating a loop which iterates over all available ports which is 65535

--- test.cpp
+++ test.cpp
@@ -1,5 +1,11 @@
 #include "pscan.hpp"
 
+void scan() {
+    for(int i = 0; i < 65535; i++){
+        
+    }
+}
+
 int main() {
     int num_threads = thread::hardware_concurrency();
     printf("Threads available: %d\n", num_threads);
