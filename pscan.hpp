9242d3be16e1a28a3c8a90003f980b227be60d8f
no-hacker-man-128 <nohackerman89@proton.me>
2024-08-23 16:42:22 +0200

    Add headerfile

---
+++ pscan.hpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#include <thread>
+#include <mutex>
+#include <vector>
+#include <algorithm>
+
+using std::thread;
+using std::mutex;
+using std::lock_guard;
+using std::vector;
+
+static vector<int> open_ports;
+
+static mutex vec_mtx;
