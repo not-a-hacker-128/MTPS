312d64ecb31919b4818710b2b0a259112669364c
no-hacker-man-128 <nohackerman89@proton.me>
2024-08-23 17:01:28 +0200

    connect to a socket,connecting local, if they could not connect or find it send a prompt, set all the memory

--- test.cpp
+++ test.cpp
@@ -2,7 +2,17 @@
 
 void scan() {
     for(int i = 0; i < 65535; i++){
-        
+        int sockfd;
+        struct sockaddr_in tower;
+
+        if (inet_pton(AF_INET,"127.0.0.1", &tower.sin_addr) < 1 ){
+            fprintf(stderr,"problem loading IP address");
+            exit(EXIT_FAILURE);
+        }
+        memset(&tower, 0, sizeof(tower));
+        tower.sin_family = AF_INET;
+        tower.sin_addr.s_addr = inet_addr("127.0.0.1");
+        tower.sin_port=htons(i);
     }
 }
 
