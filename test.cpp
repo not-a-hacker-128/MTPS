901dfb0c3ca73b105fce555b4289ce5f21fc6994
no-hacker-man-128 <nohackerman89@proton.me>
2024-08-28 15:18:04 +0200

    close socket if there is an error or else go further with code

--- test.cpp
+++ test.cpp
@@ -9,10 +9,19 @@ void scan() {
             fprintf(stderr,"problem loading IP address");
             exit(EXIT_FAILURE);
         }
+        // set memory in tower structure
         memset(&tower, 0, sizeof(tower));
         tower.sin_family = AF_INET;
         tower.sin_addr.s_addr = inet_addr("127.0.0.1");
         tower.sin_port=htons(i);
+
+        // Check due creating socket if there is an error , the socket close if there is an error 
+        // continue itteration
+        if((sockfd = socket(PF_INET,SOCK_STREAM,0)) < 0){
+            close(sockfd);
+            continue;
+
+        }
     }
 }
 
